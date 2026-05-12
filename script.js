c017e335148a311293e423b1b55bee4d6648c17c
Hafiza Naba <naba43152@gmail.com>
2026-05-12 23:34:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Enter! Please enter a valid password")
+alert("Welcome to JS Land.... \n Happy Coding");
+alert("Welcom to JS Land..");
+alert("Happy Coding!");
+alert("Hello... I can run JS through my web browser's console");
+
+
+console.log("Enter! Please enter a valid password");
+console.log("Welcome to JS Land.... \n Happy Coding");
+console.log("Happy Coding!");
+console.log("Hello... I can run JS through my web browser's console");
